23aa8d2c91ff6825efa3515cfa35a125da1b3b04
Gustavo Apolonio <gustta.apolonio@gmail.com>
2022-02-08 22:49:42 -0300

    Responsividade

--- src/app/content/content.component.ts
+++ src/app/content/content.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-content',
@@ -8,5 +8,26 @@ import { Component, OnInit } from '@angular/core';
 export class ContentComponent implements OnInit {
   constructor() {}
 
-  ngOnInit(): void {}
+  public innerWidth: any;
+  public innerHeight: any;
+
+  public fiftyWidth: any;
+  public fiftyHeight: any;
+
+  ngOnInit(): void {
+    this.innerWidth = window.innerWidth;
+    this.innerHeight = window.innerHeight;
+
+    this.fiftyWidth = this.innerWidth / 2 - 10;
+    this.fiftyHeight = (this.innerHeight - (56 + 40)) / 2 - 10;
+  }
+
+  @HostListener('window:resize', ['$event'])
+  onResize(event: any) {
+    this.innerWidth = window.innerWidth;
+    this.innerHeight = window.innerHeight;
+
+    this.fiftyWidth = this.innerWidth / 2 - 10;
+    this.fiftyHeight = (this.innerHeight - (56 + 40)) / 2 - 10;
+  }
 }
